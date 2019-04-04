3454843a7a24f9d86c21945f5843e85935145fb4
Daniel Therkildsen <dtherkildsen@hotmail.com>
2019-04-04 08:52:56 +0200

    Guestbook form error messages added

--- assets/js/guestbook.js
+++ assets/js/guestbook.js
@@ -3,9 +3,12 @@ document.addEventListener('DOMContentLoaded', () => {
     let guestbookform_nameElem = document.querySelector(".guestbookform #name");
     let guestbookform_emailElem = document.querySelector(".guestbookform #email");
     let guestbookform_messageElem = document.querySelector(".guestbookform #message");
-    let guestbook_errormsgElem = document.querySelector('.guestbook-entries__errormsg');
+    let guestbook_entriesErrorMsgElem = document.querySelector('.guestbook-entries__errormsg');
     let guestbookformThanksmsg = document.querySelector('.guestbookform__thanksmsg');
     let guestbookEntrylistElem = document.querySelector('.guestbook-entrylist');
+    let guestbookformNameErrorElem = document.querySelector('.inputfield .nameerror');
+    let guestbookformEmailErrorElem = document.querySelector('.inputfield .emailerror');
+    let guestbookformMessageErrorElem = document.querySelector('.inputfield .messageerror');
     let entryTemplate = document.querySelector('.html-templates .entry');
     var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let guestbookEntries = [];
@@ -17,7 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
             createBookEntry(entry,"after");
         });
     } else {
-        guestbook_errormsgElem.classList.remove("hidden");
+        guestbook_entriesErrorMsgElem.classList.remove("hidden");
     }
 
     guestbookformElem.addEventListener('submit', (event) => {
@@ -26,26 +29,32 @@ document.addEventListener('DOMContentLoaded', () => {
         event.preventDefault();
         if (guestbookform_nameElem.value.length >= 2 && isNaN(guestbookform_nameElem.value)) {
             guestbookform_nameElem.classList.remove("invalidfield");
+            guestbookformNameErrorElem.classList.add("hidden");
 
         } else {
             guestbookform_nameElem.classList.add("invalidfield");
+            guestbookformNameErrorElem.classList.remove("hidden");
             problem = true
         }
         if (guestbookform_emailElem.value.length > 0) {
             if (validateEmail(guestbookform_emailElem.value)) {
                 guestbookform_emailElem.classList.remove("invalidfield");
+                guestbookformEmailErrorElem.classList.add("hidden");
             } else {
-                guestbookform_emailElem.classList.add("invalidfield")
+                guestbookform_emailElem.classList.add("invalidfield");
+                guestbookformEmailErrorElem.classList.remove("hidden");
                 problem = true
             }
         } else {
             guestbookform_emailElem.classList.remove("invalidfield");
+            guestbookformEmailErrorElem.classList.add("hidden");
         }
-        if (guestbookform_messageElem.value.length > 3) {
+        if (guestbookform_messageElem.value.length >= 2) {
             guestbookform_messageElem.classList.remove("invalidfield");
-
+            guestbookformMessageErrorElem.classList.add("hidden");
         } else {
-            guestbookform_messageElem.classList.add("invalidfield")
+            guestbookform_messageElem.classList.add("invalidfield");
+            guestbookformMessageErrorElem.classList.remove("hidden");
             problem = true
         }
         if (problem != true) {
@@ -69,7 +78,7 @@ document.addEventListener('DOMContentLoaded', () => {
             guestbookEntries.unshift(guestbookEntry)
             localStorage.setItem("guestEntries", JSON.stringify(guestbookEntries));
 
-            guestbook_errormsgElem.classList.add("hidden")
+            guestbook_entriesErrorMsgElem.classList.add("hidden")
             guestbookformElem.classList.add("hidden")
             guestbookformThanksmsg.classList.remove("hidden");
             createBookEntry(guestbookEntry,"before")
